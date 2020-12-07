8044f391e0ddb5334729e2ae82a94f5d364d6af8
Tyler Sobacki <tylersobacki12@gmail.com>
2020-12-06 21:58:28 -0600

    changed ur silly buttons
    boom

--- source/sideMenu.js
+++ source/sideMenu.js
@@ -3,6 +3,7 @@ import { Text,
 	View,
 	TouchableOpacity,
 	StyleSheet,
+	Alert,
 } from 'react-native';
 
 
@@ -18,8 +19,8 @@ export default class sideMenu extends Component {
 		return(
 			<View style={styles.page}>
 				<View style={{height: 50}}></View>
-				<TouchableOpacity style={styles.button}>
-					<Text style={styles.buttonText}>Button</Text>
+				<TouchableOpacity style={styles.button} onPress={() => Alert.alert("SIKE!")}>
+					<Text style={styles.buttonText}>To new page</Text>
 				</TouchableOpacity>
 				<TouchableOpacity style={styles.button}>
 					<Text style={styles.buttonText}>Button</Text>
@@ -27,13 +28,16 @@ export default class sideMenu extends Component {
 				<TouchableOpacity style={styles.button}>
 					<Text style={styles.buttonText}>Button</Text>
 				</TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress = {()=> alert("I hate Jim!")}  >
+                		<TouchableOpacity style={styles.button} onPress = {()=> Alert.alert("I also hate jim! (but with different alert)")}  >
 					<Text style={styles.buttonText}>MyNameIsJimAndIHateMerlock</Text>
 				</TouchableOpacity>
 			</View>
 		);
 	}
 	
+	toPage = () => {
+		this.props.navigation.navigate("newPageScreen");
+	}
 }
 
 
