c60aaf69b9454658897b6436f10639d9cb094c48
Tyler Sobacki <tylersobacki12@gmail.com>
2020-12-06 20:26:31 -0600

    side menu implemented

--- source/mainScreen.js
+++ source/mainScreen.js
@@ -4,23 +4,76 @@ import { Text,
 	SafeAreaView,
 	Image,
 	TouchableOpacity,
+	StyleSheet,
+	Dimensions,
 } from 'react-native';
+import Modal from 'react-native-modal';
+
+import SideMenu from './sideMenu.js';
 
 
 
+const WIDTH = Dimensions.get("window").width;
+const HEIGHT = Dimensions.get("window").height;
 
 export default class mainScreen extends Component {
 	constructor(props: Props) {
 		super(props);
 		this.state = {
+			isSideMenuVisible: false,
 		}
 	}
 	
 	render() {
 		return 	(
 			<SafeAreaView>
-				<Text>Hello World!</Text>
+				<Modal
+					isVisible={this.state.isSideMenuVisible}
+            				onBackdropPress={this.toggleSideMenu}
+            				onSwipeComplete={this.toggleSideMenu} 
+            				animationIn="slideInLeft" 
+            				animationOut="slideOutLeft" 
+            				swipeDirection="left"
+            				style={styles.sideMenuStyle} 
+				>
+					<SideMenu/>
+				</Modal>
+				<View style={styles.header}>
+					<TouchableOpacity style={styles.fakeIcon} onPress={()=> this.toggleSideMenu()}></TouchableOpacity>
+					<Text style={styles.headerText}>Menu</Text>
+					<View style={{flex: 1}}></View>
+				</View>
 			</SafeAreaView>
 		);
 	}
+	
+	toggleSideMenu = () => {
+		this.setState({ isSideMenuVisible: !this.state.isSideMenuVisible })
+	}
 }
+
+
+const styles = StyleSheet.create({
+	header: {
+		backgroundColor: '#333333',
+		padding: 50,
+		flexDirection: 'row',
+	},
+	headerText: {
+		color: '#ffffff',
+		fontSize: 42,
+		textAlign: 'center',
+		flex: 4,
+	},
+	fakeIcon: {
+		flex: 1,
+		backgroundColor: 'blue',
+	},
+	sideMenuStyle: {
+		position: 'absolute',
+		left: 0,
+		margin: 0,
+		width: WIDTH * 0.45,
+		height: HEIGHT,
+	},
+})
---
+++ source/sideMenu.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import { Text,
+	View,
+	TouchableOpacity,
+	StyleSheet,
+} from 'react-native';
+
+
+
+export default class sideMenu extends Component {
+	constructor (props: Props) {
+		super(props);
+		this.state={
+		}
+	}
+	
+	render() {
+		return(
+			<View style={styles.page}>
+				<View style={{height: 50}}></View>
+				<TouchableOpacity style={styles.button}>
+					<Text style={styles.buttonText}>Button</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button}>
+					<Text style={styles.buttonText}>Button</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button}>
+					<Text style={styles.buttonText}>Button</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
+	
+}
+
+
+const styles = StyleSheet.create({
+	page: {
+		flex: 1,
+		backgroundColor: '#666666',
+	},
+	button: {
+		margin: 10,
+		marginTop: 15,
+		marginBottom: 15,
+		padding: 10,
+		borderWidth: 2,
+		borderColor: '#ffffff',
+		borderRadius: 4,
+	},
+	buttonText: {
+		textAlign: 'center',
+		fontSize: 20,
+	},
+	text: {
+		fontSize: 36,
+		color: 'red',
+	},
+})
